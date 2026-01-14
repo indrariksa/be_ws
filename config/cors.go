65e5ff67f138efb0486289197227160ea1411aa5
Indra Riksa <indrariksa@gmail.com>
2026-01-14 20:11:35 +0700

    added github pages cors

--- config/cors.go
+++ config/cors.go
@@ -8,7 +8,7 @@ import (
 // SetupCORS mengaktifkan middleware cors di Fiber
 func SetupCORS(app *fiber.App) {
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://127.0.0.1:5500, http://127.0.0.1:5501",
+		AllowOrigins: "http://127.0.0.1:5500, http://127.0.0.1:5501, https://indrariksa.github.io",
 		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	}))
